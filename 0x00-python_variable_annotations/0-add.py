7372b07146063900a8d5cab84deeeacd92949ec7
jabir254 <jabirkullow@gmail.com>
2023-10-08 11:25:21 +0300

    task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-"""task 0"""
+'''Task 0
+'''
 
 
-def add(a: float, b: float):
-    """ returns their sum as a float """
+def add(a: float, b: float) -> float:
+    '''Adds two floating-point numbers.
+    '''
     return a + b

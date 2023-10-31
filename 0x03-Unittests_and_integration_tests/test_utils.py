173b6e5593d76dc5a6c6c18ac069e64ec1d0ea1a
jabir254 <jabirkullow@gmail.com>
2023-10-31 20:23:30 +0300

    task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 '''Parameterize a unit test'''
 
+from typing import Dict, Tuple
 import unittest
 from utils import access_nested_map
 from parameterized import parameterized
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -20,10 +22,12 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b"),
          "Key 'b' not found in the nested map at path ['a']")
     ])
-    def test_access_nested_map_exception(self, nested_map, path, expected_message):
-        with self.assertRaises(KeyError) as context:
+    def test_access_nested_map_exception(self, nested_map: Dict, path: Tuple[str], expected_message: Exception):
+        """Tests `access_nested_map`'s exception raising."""
+        with self.assertRaises(
+            Exception
+        ) as context:
             access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), expected_message)
 
 
 if __name__ == '__main__':
